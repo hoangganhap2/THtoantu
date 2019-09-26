d67ee1770aaed2acd63fdd96b8599baf6a1335c3
Hoang <cct.cry@gmai.com>
2019-09-26 14:18:59 +0700

    up file

---
+++ THtoantu.js
@@ -0,0 +1,8 @@
+let inputwidth;
+let inputheight;
+inputwidth = prompt("nhập vào width");
+inputheight = prompt("nhập vào height ");
+let width = parseInt(inputwidth);
+let height = parseInt(inputheight);
+let area = width*height
+document.write("kết quả là : "+ area);
